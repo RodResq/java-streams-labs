f97f0fb81bbad2f17cee655aa82370f8a628f13a
RodResq <rodrigoresque@yahoo.com.br>
2021-11-24 00:38:06 -0300

    Criando a classe pojo para teste

---
+++ src/br/com/home/java/streams/Empregado.java
@@ -0,0 +1,42 @@
+package br.com.home.java.streams;
+
+@SuppressWarnings("ALL")
+public class Empregado {
+
+    private Long id;
+    private String nome;
+    private double salario;
+    private String departamento;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+}
--- src/br/com/home/java/streams/Main.java
+++ src/br/com/home/java/streams/Main.java
@@ -1,3 +1,5 @@
+package br.com.home.java.streams;
+
 public class Main {
 
     public static void main(String[] args) {
