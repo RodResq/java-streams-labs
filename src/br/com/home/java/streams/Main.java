ff02741beb99583b41a171b2f95785addd4395b2
RodResq <rodrigoresque@yahoo.com.br>
2021-11-24 00:55:00 -0300

    teste verbosidade 2

--- src/br/com/home/java/streams/Main.java
+++ src/br/com/home/java/streams/Main.java
@@ -3,11 +3,12 @@ package br.com.home.java.streams;
 import java.util.ArrayList;
 import java.util.List;
 
+@SuppressWarnings("all")
 public class Main {
 
     public static void main(String[] args) {
         List<Empregado> empregados = new ArrayList<>();
-        empregados.add(new Empregado(1L, "Joao", 100, "Producao"));
+        empregados.add(new Empregado(1L, "Joao", 2000, "Producao"));
         empregados.add(new Empregado(2L, "Maria", 3000, "RH"));
         empregados.add(new Empregado(3L, "Jose", 5000, "Controladoria"));
         empregados.add(new Empregado(4L, "Joao", 7000, "CTO"));
@@ -15,5 +16,10 @@ public class Main {
         for (Empregado emp: empregados) {
             System.out.println(emp.getNome());
         }
+        double salarioTotal = 0.0;
+        for (Empregado emp: empregados) {
+            salarioTotal = salarioTotal + emp.getSalario();
+        }
+        System.out.println("Salario total: R$ " + salarioTotal);
     }
 }
