10d0eee6944423fff817d6e7aa9f72e58adaa221
RodResq <rodrigoresque@yahoo.com.br>
2021-11-25 00:15:52 -0300

    Exemplo De Functions Lambda

--- src/br/com/home/java/streams/Main.java
+++ src/br/com/home/java/streams/Main.java
@@ -1,7 +1,11 @@
 package br.com.home.java.streams;
 
+import br.com.home.java.desafios.Pessoa;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 @SuppressWarnings("all")
 public class Main {
@@ -40,9 +44,24 @@ public class Main {
         mensageiro.emitirMessagem("Treina Web");
          */
         //criacao methodo ondFly
-        Mensageiro mensageiro = (mensagem) -> System.out.println("Mensagem da expressao Lambida: " + mensagem);
+        Mensageiro mensageiro = (mensagem) -> System.out.println("Mensagem da expressao Lambda: " + mensagem);
         mensageiro.emitirMessagem("TReina WEb");
 
+        //Consumer
+        //Entra um paramentro e nao retorna nda
+        System.out.println("*** Execucao do Consumer ***");
+        Consumer<Empregado> consumer = (emp) -> {
+            System.out.println(emp.getNome() + ", R$ " + emp.getSalario());
+        };
+        consumer.accept(new Empregado(10l, "Empregado 10", 1000, "Educacao"));
+
+        //Functions
+        System.out.println("Execucao da Function: ");
+        Function<Empregado, Double> function = emp -> emp.getSalario() * 10;
+        Function<Empregado, String> functionStr = empregado -> empregado.getNome();
+        double novoSalario = function.apply(new Empregado(1l, "EMpregado 1", 1, ""));
+        System.out.println("Novo salario: " + novoSalario);
+        System.out.println(functionStr.apply(new Empregado(11l, "Empregado 11", 2000, "Ti")));
 
     }
 }
